d7602db3eaf25197b40f50aec7f9047352e55a4d
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2021-01-03 05:04:23 +0600

    .....

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,24 +1,51 @@
+
+const sha256=require('sha256');
 function Blockchain()
 {
     this.chain=[]
-    this.newTransactions=[] 
+    this.pendingTransactions=[] 
 }
 Blockchain.prototype.createNewBlock=function(nonce,previousBlockHash,hash) 
 {
     const newBloclk = {
         index:this.chain.length+1,
         timestamp:Date.now(),
-        transaction:this.newTransactions,
+        transactions:this.pendingTransactions,
         nonce: nonce,      
         hash:hash,
         previousBlockHash:previousBlockHash,
 
     };
-    this.newTransactions=[];
+    this.pendingTransactions=[];
+
     this.chain.push(newBloclk);
     return newBloclk;
   
-    
+
+}
+
+Blockchain.prototype.getLastBlock=function()
+{
+    return this.chain[this.chain.length-1];
+
+}
+Blockchain.prototype.createNewTransaction= function(amount,sender,reciepient)
+{
+    const newTransaction={
+        amount:amount,
+        sender:sender,
+        reciepient:reciepient
+
+    };
+    this.pendingTransactions.push(newTransaction);
+    return this.getLastBlock()['index']+1;
+
+}
+Blockchain.prototype.hashBlock =function(previousBlockHash,currentBlockData,nonce)
+{
+    const dataAsString=previousBlockHash+nonce.toString()+JSON.stringify(currentBlockData);
+    const hash= sha256(dataAsString);
+    return hash;
 
 
 }
--- dev/test.js
+++ dev/test.js
@@ -1,6 +1,8 @@
 const Blockchain=require('./blockchain');
 const bitcoin = new Blockchain();
 bitcoin.createNewBlock(23232,'122KJK3423442','198HKH32323234')
-bitcoin.createNewBlock(23233,'122K23K3423442','198HKH32323277')
+//bitcoin.createNewBlock(23233,'122K23K3423442','198HKH32323277')
+bitcoin.createNewTransaction(300,'kdk893434','74389783497')
+bitcoin.createNewBlock(23452,'3333134322KJK3423442','33333198HKH32323234')
 
-console.log(bitcoin)
+console.log(bitcoin);
