082b4356715c741f4edc48ae7bb318cdb362fa57
Reyad Hasan Rinkon <45186655+rinkon005@users.noreply.github.com>
2021-01-05 04:44:29 +0600

    .....

---
+++ dev/api.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/blockchain', function (req, res) {
+
+
+
+});
+
+app.post('/transaction',function(req,res){
+
+
+});
+app.get('/mine',function(req,res){
+
+
+});
+
+
+app.listen(3005,function()
+{console.log('Listining to port 3005')}
+);
